60ac903109ddb7a32c423b372d5aa76da2ac54b9
marziehkamali <mayhey1@gmail.com>
2023-08-30 06:55:23 -0600

    code changed to reflect fetch geo coordinates with IP

--- iss.js
+++ iss.js
@@ -1,5 +1,6 @@
 const request = require('request');
-const fetchMyIP = function(callback) {
+
+/* const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) return callback(error, null);
 
@@ -11,6 +12,31 @@ const fetchMyIP = function(callback) {
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
+}; */
+
+//module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(ip, callback) {
+request(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+if (error) {
+  callback(error, null);
+  return;
+}
+
+const parsedBody = JSON.parse(body);
+
+if (!parsedBody.success) {
+  const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+  callback(Error(message), null);
+  return;
+} 
+
+const { latitude, longitude } = parsedBody;
+
+callback(null, {latitude, longitude});
+});
 };
 
-module.exports = { fetchMyIP };
+// Don't need to export the other function since we are not testing it right now.
+module.exports = { fetchCoordsByIP };
